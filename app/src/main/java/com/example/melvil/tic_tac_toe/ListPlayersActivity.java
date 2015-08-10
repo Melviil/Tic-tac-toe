3305a64728ec286373de9d22cd4ed3ae8b18aa8a
Samuel Batissou <samuel.batissou@etu.unilim.fr>
2015-08-10 13:44:49 +0200

    Update ServiceConnectDb

--- app/src/main/java/com/example/melvil/tic_tac_toe/ListPlayersActivity.java
+++ app/src/main/java/com/example/melvil/tic_tac_toe/ListPlayersActivity.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.StrictMode;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.net.URL;
 import java.util.Map;
@@ -16,6 +19,7 @@ import java.util.Map;
  * Created by samuel on 09/08/15.
  */
 public class ListPlayersActivity extends Activity {
+    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
     Service_ConnectToDB service_connectToDB;
     ProgressBar progressBar;
     ListView listView;
@@ -37,6 +41,13 @@ public class ListPlayersActivity extends Activity {
         setContentView(R.layout.activity_listplayers);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         listView = (ListView) findViewById(R.id.listView);
+        progressBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Hey",Toast.LENGTH_LONG).show();
+                service_connectToDB.getNamesAndIp();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/melvil/tic_tac_toe/Service_ConnectToDB.java
+++ app/src/main/java/com/example/melvil/tic_tac_toe/Service_ConnectToDB.java
@@ -2,17 +2,21 @@ package com.example.melvil.tic_tac_toe;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Binder;
 import android.os.IBinder;
-import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.Map;
 
 /**
@@ -21,45 +25,67 @@ import java.util.Map;
 public class Service_ConnectToDB extends Service {
     //TODO Finish this class
     Map<String, URL> namesAndIp;
-    URL abbaye;
+    URL removeLinesOnGames;
+    URL getNamesAndIP;
+    URL addLineOnGames;
 
     public Service_ConnectToDB() {
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
+    public void onCreate() {
+        super.onCreate();
         try {
-            abbaye = new URL("abbaye.noip.me");
+            this.getNamesAndIP = new URL("http://abbaye.noip.me/Android/getNamesandIP.php");
+            this.addLineOnGames = new URL("http://abbaye.noip.me/Android/addLineOnGames.php");
+            this.removeLinesOnGames = new URL("http://abbaye.noip.me/Android/removeLineOnGames.php");
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
         return new MyBinder();
+    }
+
+    //TODO verify this code
+    public Map<String, URL> getNamesAndIp() {
+        //TODO Need to receive Database inforamtion for the listview -> Coreect this méthod
+        AsyncTask task = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] params) {
+                HttpClient client = new DefaultHttpClient();
+                HttpGet request = new HttpGet();
+                HttpResponse response = null;
+                try {
+                    request.setURI(getNamesAndIP.toURI());
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    response = client.execute(request);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (response != null) {
+                    try {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return null;
+            }
+
+        };
+
+        return null;
     }
 
     public class MyBinder extends Binder {
         public Service_ConnectToDB getMyService() {
-            return new Service_ConnectToDB();
+            return Service_ConnectToDB.this;
         }
-    }
-    //TODO verify this code
-    public Map<String, URL> getNamesAndIp() {
-        try {
-            HttpURLConnection urlConnection = (HttpURLConnection)abbaye.openConnection();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-            StringBuilder stringBuilder = new StringBuilder();
-            int cp;
-            try {
-                while ((cp = reader.read()) != -1) {
-                    stringBuilder.append((char) cp);
-                }
-                String json=stringBuilder.toString();
-                Toast.makeText(getApplicationContext(),json,Toast.LENGTH_SHORT).show();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return namesAndIp;
     }
 }
