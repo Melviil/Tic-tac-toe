cb240b37847ac92aa7f4c17c8e3565d333d5f63d
Samuel Batissou <samuel.batissou@etu.unilim.fr>
2015-08-12 21:16:10 +0200

    All script work except GetBlob and PostBlob 75%
    And update of scripts php

--- app/src/main/java/php/joinAGame.php
+++ app/src/main/java/php/joinAGame.php
@@ -8,8 +8,20 @@ if (mysqli_connect_errno($con))
 }
 
 $idPlayer1 = $_POST['idPlayer1'];
-$idPlayer2 = $_POST['idPlayer2'];
-$sql = "UPDATE Game SET idPlayer2=$idPlayer2 WHERE idPlayer1 = $idPlayer2";
+$namePlayer2 = $_POST['name'];
+//$idPlayer1 = $_GET['idPlayer1'];
+//$namePlayer2 = $_GET['name'];
+echo "idPlayer1 = $idPlayer1";
+echo "name Player2 = $namePlayer2";
+
+$result = mysqli_query($con,"SELECT idPlayers FROM Players WHERE name=\"$namePlayer2\"");
+while($row = mysqli_fetch_array($result)){
+    $data[]=$row[0];
+}
+
+$idPlayer2 = $data[0];
+echo "idPlayer2 = $idPlayer2";
+$sql = "UPDATE Game SET idPlayer2=$idPlayer2 WHERE idPlayer1=$idPlayer1";
 mysqli_query($con,$sql);
 
 mysqli_close($con);
