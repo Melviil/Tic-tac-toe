d75b557f69d9a08a65758b4b61988542e6ccb989
Samuel Batissou <samuel.batissou@etu.unilim.fr>
2015-08-12 15:42:03 +0200

    Updatating tasks

--- app/src/main/java/com/example/melvil/tic_tac_toe/TaskGetLineOfTheWeb.java
+++ app/src/main/java/com/example/melvil/tic_tac_toe/TaskGetLineOfTheWeb.java
@@ -1,6 +1,11 @@
 package com.example.melvil.tic_tac_toe;
 
 import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -12,45 +17,52 @@ import java.net.URL;
 /**
  * Created by samuel on 12/08/15.
  */
-public class TaskGetLineOfTheWeb extends AsyncTask<Object,Void,Object> {
+public class TaskGetLineOfTheWeb extends AsyncTask<Object, Void, Object> {
 
     @Override
     protected Object doInBackground(Object... params) {
+        Object[] result = new Object[2];
         HttpURLConnection connection;
         URL UrlGetLineOfGame = (URL) params[0];
-        Integer idPlayer = (Integer) params[1];
-        String data = "id=" + id;
-        try {
-            connection = (HttpURLConnection) UrlGetLineOfGame.openConnection();
-            connection.setDoOutput(true);
-            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            connection.setRequestMethod("POST");
+        Integer idPlayer1 = (Integer) params[1];
+        String data = "idPlayer1=" + idPlayer1;
+        Boolean finish = false;
+        while (!finish) {
+            try {
+                connection = (HttpURLConnection) UrlGetLineOfGame.openConnection();
+                connection.setDoOutput(true);
+                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                connection.setRequestMethod("POST");
 
-            DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
-            outputStream.writeBytes(data);
-            outputStream.flush();
-            outputStream.close();
+                DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+                outputStream.writeBytes(data);
+                outputStream.flush();
+                outputStream.close();
 
-            int responseCode = connection.getResponseCode();
-            System.out.println("\nSending 'POST' request to URL : " + UrlGetLineOfGame);
+                Integer responseCode = connection.getResponseCode();
+                Log.d("Reponse code", responseCode.toString());
 
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            StringBuilder response = new StringBuilder();
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
 
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                JSONArray jsonArray = new JSONArray(response.toString());
+                JSONObject jsonObject = jsonArray.getJSONObject(0);
+                if (jsonObject.getString("idPlayer2") != null) {
+                    finish = true;
+                    result[0] = true;
+                    result[1] = jsonObject;
+                }
+                in.close();
+                connection.disconnect();
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
             }
-            in.close();
-
-            //print result
-            System.out.println(response.toString());
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-        return true;
+        return result;
     }
 }
--- app/src/main/java/com/example/melvil/tic_tac_toe/TaskJoinAGame.java
+++ app/src/main/java/com/example/melvil/tic_tac_toe/TaskJoinAGame.java
@@ -1,7 +1,41 @@
 package com.example.melvil.tic_tac_toe;
 
+import android.os.AsyncTask;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by samuel on 12/08/15.
  */
-public class TaskJoinAGame {
+public class TaskJoinAGame extends AsyncTask<Object,Void,Void> {
+    @Override
+    protected Void doInBackground(Object... params) {
+        HttpURLConnection connection;
+        URL urlJoinAGame = (URL) params[0];
+        Integer idPlayer1 = (Integer) params[1];
+        Integer idPlayer2 = (Integer) params[2];
+        String data = "idPlayer"+idPlayer1+"&idPlayer2"+idPlayer2;
+        try {
+            connection = (HttpURLConnection) urlJoinAGame.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestMethod("POST");
+
+            DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+            outputStream.writeBytes(data);
+            outputStream.flush();
+            outputStream.close();
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response Code : " + responseCode);
+
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
