2975a563b8637e6033862c708d783ae7982e84b7
Samuel Batissou <samuel.batissou@etu.unilim.fr>
2015-08-12 16:36:31 +0200

    Upgrading..

--- app/src/main/java/com/example/melvil/tic_tac_toe/ListPlayersActivity.java
+++ app/src/main/java/com/example/melvil/tic_tac_toe/ListPlayersActivity.java
@@ -67,7 +67,7 @@ public class ListPlayersActivity extends Activity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                service_connectToDB.joinAGame(name,getKeyFromValue(players,adapter.getItem(position)));
             }
         });
         create.setOnClickListener(new View.OnClickListener() {
