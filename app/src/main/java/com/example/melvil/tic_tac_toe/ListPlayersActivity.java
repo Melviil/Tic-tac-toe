a92c2d0d509cbcb8ff1aa35d8a10dfa8885553d2
Samuel Batissou <samuel.batissou@etu.unilim.fr>
2015-08-12 16:01:34 +0200

    Update

--- app/src/main/java/com/example/melvil/tic_tac_toe/ListPlayersActivity.java
+++ app/src/main/java/com/example/melvil/tic_tac_toe/ListPlayersActivity.java
@@ -66,7 +66,6 @@ public class ListPlayersActivity extends Activity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //TODO Compltete this.
 
             }
         });
