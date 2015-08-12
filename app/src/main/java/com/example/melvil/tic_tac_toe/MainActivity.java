0ecd1fc46d10ae763662528eb675b9fa65410443
davidkocnar <davidkocnar@seznam.cz>
2015-08-12 14:31:30 +0200

    added Shared Preferences of player name

--- app/src/main/java/com/example/melvil/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/example/melvil/tic_tac_toe/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.melvil.tic_tac_toe;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +18,8 @@ public class MainActivity extends Activity {
     Button play;
     Boolean choice;
     Button multilayer;
+    public static String PACKAGE_NAME;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,9 @@ public class MainActivity extends Activity {
         multilayer = (Button) findViewById(R.id.multi);
         choice = true;
         final String no_name = getResources().getString(R.string.no_name);
+        PACKAGE_NAME = getApplicationContext().getPackageName();
+        prefs = getSharedPreferences(PACKAGE_NAME, Context.MODE_PRIVATE);
+        et.setText(prefs.getString(PACKAGE_NAME, ""));
 
         /*
         Click listener for the multiplayer button, starts an Intent of ListPLayersActivity
@@ -37,6 +44,7 @@ public class MainActivity extends Activity {
                 if (et.getText().toString().equals("")) {
                     Toast.makeText(getApplicationContext(), no_name, Toast.LENGTH_SHORT).show();
                 } else {
+                    prefs.edit().putString(PACKAGE_NAME, et.getText().toString()).apply();
                     Intent intent = new Intent(getApplicationContext(), ListPlayersActivity.class);
                     intent.putExtra("name", et.getText().toString());
                     startActivity(intent);
@@ -50,6 +58,7 @@ public class MainActivity extends Activity {
                 if (et.getText().toString().equals("")) {
                     Toast.makeText(getApplicationContext(), no_name, Toast.LENGTH_SHORT).show();
                 } else {
+                    prefs.edit().putString(PACKAGE_NAME, et.getText().toString()).apply();
                     Intent intent = new Intent(MainActivity.this, GameActivity.class);
                     intent.putExtra("name", et.getText().toString());
                     startActivity(intent);
